9debb01dba15f3dc5ced7bb603312bf861ea2b22
Bo <44844728+Robobo2022@users.noreply.github.com>
2023-11-14 22:59:11 +0100

    update
    erm setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='redapi',
+    version='1.0',
+    description='Erm reddit api wrapper',
+    packages=['redapi'],
+    author='Bovanlaarhoven',
+    zip_safe=False)
---
+++ src/README.md
@@ -0,0 +1,2 @@
+# Instruction
+erm erm erm
